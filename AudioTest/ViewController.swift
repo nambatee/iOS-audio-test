e396fa2ab8836828538aa06226f2d2a9c16c81e1
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-16 09:38:42 -0400

    Initial Commit

---
+++ AudioTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioTest
+//
+//  Created by Natalia Harateh on 8/16/18.
+//  Copyright © 2018 Natalia Harateh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
